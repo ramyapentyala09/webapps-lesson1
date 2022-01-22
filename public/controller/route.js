0cd24315688b7df571471fe521fd0579a4b4eeb1
ramyapentyala <ramyapentyala@gmail.com>
2022-01-22 15:02:53 -0600

    32

--- public/controller/route.js
+++ public/controller/route.js
@@ -1,17 +1,20 @@
 import { about_page } from "../viewpage/about_page.js"
 import { home_page } from "../viewpage/home_page.js"
 import { thread_page } from "../viewpage/thread_page.js";
+import { search_page } from "../viewpage/search_page.js";
 
 export const routePath = {
     HOME: '/',
     ABOUT: '/about',
     THREAD: '/thread',
+    SEARCH: '/search',
 }
 
 export const routes = [
     { path: routePath.HOME, page: home_page },
     { path: routePath.ABOUT, page: about_page },
     { path: routePath.THREAD, page: thread_page },
+    { path: routePath.SEARCH, page: search_page },
 ];
 
 export function routing(pathname, hash) {
--- public/viewpage/search_page.js
+++ public/viewpage/search_page.js
@@ -1,9 +1,28 @@
 import * as Elements from './elements.js'
+import * as Util from './util.js'
+import { routePath } from '../controller/route.js';
+import { currentUser } from '../controller/firebase_auth.js';
+import * as ProtectedMessage from './protected_message.js'
 
 export function addEventListeners() {
 
 Elements.formSearch.addEventListener('submit', e => {
     e.preventDefault();
     const searchKeys = e.target.searchKeys.value.trim();
+if (searchKeys.length == 0) {
+    Util.info('Error', 'No search keys');
+    return;
+}
+const searchKeysArray = searchKeys.toLowerCase().match(/\S+/g);
+const joinedSearchKeys = searchKeysArray.join(',');
+history.pushState(null, null, routePath.SEARCH + '#' + joinedSearchKeys);
+search_page(joinedSearchKeys)
 });
+}
+export function search_page(joinedSearchKeys) {
+if (!currentUser) {
+    Elements.root.innerHTML = ProtectedMessage.html;
+    return;
+}
+Elements.root.innerHTML = `<h1>Search for: ${joinedSearchKeys}</h1>`;
 }
